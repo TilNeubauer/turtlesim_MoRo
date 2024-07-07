6b0c298e324180f71f7e699b489195eb7bb39805
TilNeubauer <nt1703@mci4me.at>
2024-07-07 14:58:03 +0200

    .bsh angepasst

--- turtlesim_mo_ro.bash
+++ turtlesim_mo_ro.bash
@@ -1,42 +1,48 @@
 #!/bin/bash
 
-#init var
+# Init variables
 WORKSPACE_DIR=~/ros2_ws
 SRC_DIR=$WORKSPACE_DIR/src
 PACKAGE_NAME=turtlesim_mo_ro
 DOWNLOADS_DIR=~/Downloads
-ZIP_FILE=$DOWNLOADS_DIR/turtlesim_mo_ro.zip
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-#mk workspace
+# Create workspace directory
 mkdir -p $SRC_DIR
 
-# dateien zu projekt
+# Move package to the src directory
 if [ -d $DOWNLOADS_DIR/$PACKAGE_NAME ]; then
     mv $DOWNLOADS_DIR/$PACKAGE_NAME $SRC_DIR/
 fi
 
-#Compile von pkg
+# Clone turtlesim if not already in the workspace
+if [ ! -d $SRC_DIR/ros_tutorials ]; then
+    cd $SRC_DIR
+    git clone https://github.com/ros/ros_tutorials.git
+fi
+
+# Compile the package
 cd $WORKSPACE_DIR
 colcon build --packages-select $PACKAGE_NAME --allow-overriding turtlesim
 if [ $? -eq 0 ]; then
     source install/setup.bash
 
-    #settup script + starten von turtlesim_node
+    # Start turtlesim_node in a new terminal
     gnome-terminal -- bash -c "
     source /opt/ros/humble/setup.bash
+    source $WORKSPACE_DIR/install/setup.bash
     ros2 run turtlesim turtlesim_node
     " &
     sleep 4
 
-    #run pkg
+    # Run your package in another new terminal
     gnome-terminal -- bash -c "
     source /opt/ros/humble/setup.bash
     source $WORKSPACE_DIR/install/setup.bash
     ros2 run $PACKAGE_NAME turtlesim_mo_ro
     "
 
-    #rm 
+    # Clean up
     rm -rf $DOWNLOADS_DIR/$PACKAGE_NAME
 else
     echo "Build failed. Please check the build logs for errors."
